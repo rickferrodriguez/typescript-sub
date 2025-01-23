198b92e493869c276485fdc69791c35e1b381449
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2025-01-23 17:14:30 -0500

    assigning a type to a variable

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 console.log("Hola, TypeScript!");
 
-interface;
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  isAdmin: boolean;
+}
+
+// assigning types to variables
+const defaultUser: User = {
+  id: 0,
+  firstName: "Richard",
+  lastName: "Rodriguez",
+  isAdmin: true,
+};
+
+const getUserId = (user: User) => {
+  return user.id;
+};
+
+getUserId(defaultUser);
